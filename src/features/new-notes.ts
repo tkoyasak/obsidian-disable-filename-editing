ba395b9cb312c5f567d34c1547a54b31711c0453
tkoyasak <72780548+tkoyasak@users.noreply.github.com>
2024-12-07 06:00:24 +0900

    feat: remove diary template

--- src/features/new-notes.ts
+++ src/features/new-notes.ts
@@ -1,4 +1,4 @@
-import { type App, moment, normalizePath, Notice } from 'obsidian'
+import { type App, normalizePath, Notice } from 'obsidian'
 import { ulid } from 'ulid'
 import { v7 as uuid } from 'uuid'
 import { Settings } from '../settings.ts'
@@ -13,7 +13,7 @@ export class NewNotes {
    * Create a new note with a unique id.
    */
   public async createNewNote(): Promise<void> {
-    const id = generateUID(this.settings.uidType)
+    const id = UID_CATALOG[this.settings.uidType as UIDType]()
     const folder = this.app.fileManager.getNewFileParent('').path
     const path = normalizePath(`${folder}/${id}.md`)
 
@@ -22,29 +22,20 @@ export class NewNotes {
       return
     }
 
-    const file = await this.app.vault.create(path, this.newEntry(id))
+    const file = await this.app.vault.create(path, entry(id))
     const leaf = this.app.workspace.getLeaf()
     await leaf.openFile(file)
-  }
-
-  private newEntry(id: string): string {
-    return this.settings.uidType !== 'diary' ? uniqueEntry(id) : diaryEntry(id)
   }
 }
 
 export const UID_CATALOG = {
   ulid,
   uuid,
-  diary: () => moment().format('YYYYMM'),
 } as const
 
 type UIDType = keyof typeof UID_CATALOG
 
-function generateUID(type: string): string {
-  return UID_CATALOG[type as UIDType]()
-}
-
-function uniqueEntry(id: string): string {
+function entry(id: string): string {
   return `---
 id: ${id}
 created_at:
@@ -54,29 +45,4 @@ tags: []
 ---
 
 `
-}
-
-const DOW = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'] as const
-
-function diaryEntry(id: string): string {
-  let content = `---
-id: ${id}
-created_at:
-modified_at:
----
-`
-  const m = moment(id, 'YYYYMM')
-  const n = m.daysInMonth()
-  let w = Number.parseInt(m.format('d'), 10)
-  for (let i = 1; i <= n; i++) {
-    const dd = i.toString().padStart(2, '0')
-    const ddd = DOW[w]
-    content += `
-###### ${id}${dd}${ddd}
-
-
-`
-    w = (w + 1) % 7
-  }
-  return content
 }
